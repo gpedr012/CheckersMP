483cd568a4f554de3e6129788d8f6c11e5079204
Gessler Pedrol <gpedr012@fiu.edu>
2020-05-23 16:54:45 -0400

    Board adds tiles correctly.

--- src/sample/Board.java
+++ src/sample/Board.java
@@ -1,24 +1,38 @@
 package sample;
 
-public class Board
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
+
+
+public class Board extends GridPane
 {
     private final int NUM_ROWS = 8, NUM_COLS = 8;
 
-    Tile[][] tiles = new Tile[NUM_ROWS][NUM_COLS];
+    private Tile[][] tiles = new Tile[NUM_ROWS][NUM_COLS];
+
 
     public Board()
     {
-
+        initTiles();
 
     }
 
     private void initTiles()
     {
+        boolean currentColor = true;
 
-    }
+        for (int i = 0; i < tiles.length; i++)
+        {
+            for (int j = 0; j < tiles[0].length ; j++)
+            {
+                tiles[i][j] = new Tile(currentColor);
+                add(tiles[i][j], i, j);
+                currentColor = !currentColor;
 
-    private void initPane()
-    {
+            }
+
+            currentColor = !currentColor;
+        }
 
     }
 
