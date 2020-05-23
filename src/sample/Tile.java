3d0722b76b3ac7361f73fc0853c7cd9b5ce2c693
Gessler Pedrol <gpedr012@fiu.edu>
2020-05-23 16:26:21 -0400

    Added constructor. Added bool to keep track if a piece is on that tile.

--- src/sample/Tile.java
+++ src/sample/Tile.java
@@ -1,6 +1,32 @@
 package sample;
 
-public class Tile
-{
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
+public class Tile extends StackPane
+{
+    private final int sideLength = 40;
+
+    private Rectangle tileBackground = new Rectangle(sideLength, sideLength);
+    private boolean containsPiece = false;
+
+
+    public Tile(boolean colorBool)
+    {
+        //true = white tile, false = black tile.
+        Color color = colorBool ? Color.WHITE : Color.BLACK;
+        tileBackground.setFill(color);
+
+    }
+
+    public boolean containsPiece()
+    {
+        return containsPiece;
+    }
+
+    public void setContainsPiece(boolean containsPiece)
+    {
+        this.containsPiece = containsPiece;
+    }
 }
