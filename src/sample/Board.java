a43f10a9de1ae2ae80d9bd475b7ac2a8d7bfe1d2
Gessler Pedrol <gpedr012@fiu.edu>
2020-05-23 16:27:23 -0400

    Added methods that might be needed.

---
+++ src/sample/Board.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Board
+{
+    private final int NUM_ROWS = 8, NUM_COLS = 8;
+
+    Tile[][] tiles = new Tile[NUM_ROWS][NUM_COLS];
+
+    public Board()
+    {
+
+
+    }
+
+    private void initTiles()
+    {
+
+    }
+
+    private void initPane()
+    {
+
+    }
+
+
+
+}
