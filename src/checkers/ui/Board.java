006722640691ae2ac5f822f22d6f02f978e3032d
Gessler Pedrol <gpedr012@fiu.edu>
2020-06-12 05:14:55 -0400

    Refactoring work.

--- src/checkers/ui/Board.java
+++ src/checkers/ui/Board.java
@@ -1,13 +1,10 @@
 package checkers.ui;
 
 import checkers.player.Player;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
-import java.io.InvalidObjectException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 //TODO: Setup changes depending on whether 1 player plays or two.
 //If one player, always put player's pieces at the bottom of the board.
@@ -15,76 +12,70 @@ import java.util.Stack;
 
 public class Board extends GridPane
 {
-    private final int NUM_ROWS = 8, NUM_COLS = 8, NUM_DARK_TILES = 32, NUM_DARK_TILES_PER_ROW = 4;
+    private final int NUM_ROWS = 8, NUM_COLS = 8;
 
-    private List<List<Tile>> darkTiles = new ArrayList<List<Tile>>(NUM_DARK_TILES);
+    private List<List<Tile>> tiles = new ArrayList<List<Tile>>(NUM_ROWS * NUM_COLS);
 
 
     public Board(Player playerOne, Player playerTwo)
     {
         initTiles();
-        initPieces(playerOne);
-        initPieces(playerTwo);
-
-
-
+        addPiecesToPlayer(playerOne);
+        addPiecesToPlayer(playerTwo);
     }
+
 
 
     private void initTiles()
     {
-        //this boolean helps switch between the colors since they alternate between rows and cols.
         boolean colorAlternator = true;
-        //The piece that is on the edge alternates from being first to last on each row.
-        //true = 1st tile is edge, false = last tile is edge.
         boolean edgeAlternator = false;
         int darkTileCounter = -1;
 
         for (int i = 0; i < NUM_ROWS; i++)
         {
-            darkTiles.add(new ArrayList<>(NUM_DARK_TILES_PER_ROW));
+            tiles.add(new ArrayList<>(NUM_COLS));
 
             for (int j = 0; j < NUM_COLS ; j++)
             {
                 Tile tile = colorAlternator ? Tile.createLightTile() : Tile.createDarkTile();
                 add(tile, j, i);
-                if(!colorAlternator)
-                {
-                    darkTiles.get(i).add(tile);
-
-
-                }
-
+                tiles.get(i).add(tile);
                 colorAlternator = !colorAlternator;
             }
 
-            if (edgeAlternator)
-            {
-                darkTiles.get(i).get(0).setEdge(true);
-            } else
-            {
-                darkTiles.get(i).get(NUM_DARK_TILES_PER_ROW - 1).setEdge(true);
-            }
-            edgeAlternator = !edgeAlternator;
             colorAlternator = !colorAlternator;
         }
     }
 
 
-    private void initPieces(Player player)
+
+    public void addPiecesToPlayer(Player player)
     {
         int startingRow = player.getPlayerNumber() == 1 ? NUM_ROWS - 3 : 0;
 
         for (int i = startingRow; i < startingRow + 3; i++)
         {
-            for (int j = 0; j < darkTiles.get(i).size(); j++)
+            for (int j = 0; j < tiles.get(i).size(); j++)
             {
-                darkTiles.get(i).get(j).getChildren().add(new Piece(player.getPlayerColor(), i, j));
+                if(tiles.get(i).get(j).getColor() == Tile.DARK_COLOR)
+                {
+                    Piece piece = new Piece(player.getPlayerColor(), i, j);
+                    player.addPiece(piece);
+                    tiles.get(i).get(j).addPiece(piece);
+                }
+
             }
         }
-
-
     }
 
+    public List<List<Tile>> getTiles()
+    {
+        return tiles;
+    }
 
+    public Tile getTile(int row, int col)
+    {
+        return tiles.get(row).get(col);
+    }
 }
