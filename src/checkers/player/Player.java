c129b76bfadd2d2abe074de8e3bcd0cd8b4ca43b
Gessler Pedrol <gpedr012@fiu.edu>
2020-06-12 05:13:46 -0400

    Changed from interface to abstract class to better reflect functinoality.

--- src/checkers/player/Player.java
+++ src/checkers/player/Player.java
@@ -1,27 +1,75 @@
 package checkers.player;
 
 import checkers.ui.Piece;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Player
+public abstract class Player
 {
 
-    void addPiece(Piece piece);
+    List<Piece> piecesList;
+    final Piece.PieceColor playerColor;
+    final int playerNumber;
+    BooleanProperty hasTurn = new SimpleBooleanProperty(false);
 
-    List<Piece> getPiecesWithMoves();
+    public Player(List<Piece> piecesList, int playerNumber)
+    {
+        playerColor = piecesList.get(0).getColor();
+        this.playerNumber = playerNumber;
+        this.piecesList = piecesList;
+    }
 
-    boolean hasPiecesLeft();
+    public void addPiece(Piece piece)
+    {
+        piecesList.add(piece);
+    }
 
-    int getPlayerNumber();
+    public List<Piece> getPiecesWithMoves()
+    {
+        List<Piece> piecesWithMoves = new ArrayList<>();
+        for (Piece piece : piecesList)
+        {
+            if(piece.hasAnyMoves())
+            {
+                piecesWithMoves.add(piece);
+            }
+        }
 
-    Piece.PieceColor getPlayerColor();
+        return piecesWithMoves;
+    }
 
+    public boolean hasPiecesLeft()
+    {
+        return piecesList.isEmpty();
+    }
 
+    public int getPlayerNumber()
+    {
+        return playerNumber;
+    }
 
+    public Piece.PieceColor getPlayerColor()
+    {
+        return playerColor;
+    }
 
+    public boolean getHasTurn()
+    {
+        return hasTurn.get();
+    }
 
+    public BooleanProperty hasTurnProperty()
+    {
+        return hasTurn;
+    }
 
+    public void setHasTurn(boolean hasTurn)
+    {
+        this.hasTurn.set(hasTurn);
+    }
 
 
 }
