cf344a176f1073c393247b84812ca68835d87711
Gessler Pedrol <gpedr012@fiu.edu>
2020-06-12 05:14:42 -0400

    Modified to reflect change of changing Player from interface to parent class.

--- src/checkers/player/HumanPlayer.java
+++ src/checkers/player/HumanPlayer.java
@@ -1,56 +1,24 @@
 package checkers.player;
 
+import checkers.ui.Board;
 import checkers.ui.Piece;
+import javafx.beans.binding.BooleanBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class HumanPlayer implements Player
+public class HumanPlayer extends Player
 {
     private List<Piece> pieces = new ArrayList<>(12);
     private int playerNumber;
     private Piece.PieceColor playerColor;
+    private Board board;
+    private MoveCalculator moveCalculator;
 
-    public HumanPlayer(int playerNumber, Piece.PieceColor playerColor)
+
+    public HumanPlayer(List<Piece> piecesList, int playerNumber)
     {
-        this.playerNumber = playerNumber;
-        this.playerColor = playerColor;
-    }
-
-    @Override
-    public void addPiece(Piece piece)
-    {
-        pieces.add(piece);
-    }
-
-    @Override
-    public List<Piece> getPiecesWithMoves()
-    {
-        List<Piece> piecesWithMoves = new Stack<>();
-        for (Piece piece: pieces
-             )
-        {
-
-        }
-        return null;
-    }
-
-    @Override
-    public boolean hasPiecesLeft()
-    {
-        return pieces.isEmpty();
-    }
-
-    @Override
-    public int getPlayerNumber()
-    {
-        return playerNumber;
-    }
-
-    @Override
-    public Piece.PieceColor getPlayerColor()
-    {
-        return playerColor;
+        super(piecesList, playerNumber);
     }
 }
