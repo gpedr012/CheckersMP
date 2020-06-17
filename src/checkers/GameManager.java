1f4e458ce3930b68c70ec34fa579948cbb2844e9
Gessler Pedrol <gpedr012@fiu.edu>
2020-06-17 10:11:41 -0400

    Correctly makes the game start and progress.

--- src/checkers/GameManager.java
+++ src/checkers/GameManager.java
@@ -1,6 +1,71 @@
 package checkers;
 
+import checkers.player.Player;
+import checkers.ui.Board;
+import checkers.ui.Piece;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
+import java.util.List;
+
 public class GameManager
 {
+    private Board board;
+    private Player playerOne;
+    private Player playerTwo;
+
+    public GameManager(Board board, Player playerOne, Player playerTwo)
+    {
+        this.board = board;
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        initListeners();
+    }
+
+    public void startGame()
+    {
+        playerOne.processTurn();
+
+    }
+
+    private void initListeners()
+    {
+        playerOne.hasTurnProperty().addListener(new ChangeListener<Boolean>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldV, Boolean newV)
+            {
+                if(newV)
+                {
+                    playerOne.processTurn();
+                }
+                else
+                {
+                    playerTwo.processTurn();
+                }
+            }
+        });
+
+        playerTwo.hasTurnProperty().addListener(new ChangeListener<Boolean>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldV, Boolean newV)
+            {
+                if(newV)
+                {
+                    playerTwo.processTurn();
+                }
+                else
+                {
+                    playerOne.processTurn();
+                }
+            }
+        });
+
+
+    }
+
 
 }
