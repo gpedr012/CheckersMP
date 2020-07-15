c031e40f6c63222ec9e02563ec4e547892f90dac
Gessler Pedrol <gpedr012@fiu.edu>
2020-07-15 18:52:54 -0400

    Started making main menu.

--- src/checkers/MainMenu.java
+++ src/checkers/MainMenu.java
@@ -1,7 +1,28 @@
 package checkers;
 
-public class MainMenu
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application
 {
 
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("mainmenu.fxml"));
+        stage.setScene(new Scene(root, 500, 500));
+        stage.setTitle("Checkers Game");
+        stage.show();
 
+
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 }
--- src/checkers/MenuController.java
+++ src/checkers/MenuController.java
@@ -1,5 +1,31 @@
 package checkers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MenuController
 {
+    @FXML
+    Button localBtn;
+    @FXML
+    Button onlineBtn;
+    @FXML
+    Button quitBtn;
+
+    public void handleLocalBtn()
+    {
+
+    }
+
+    public void handleOnlineBtn()
+    {
+
+    }
+
+    public void handleQuitBtn()
+    {
+
+
+    }
+
 }
