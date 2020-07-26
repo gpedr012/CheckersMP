9e75c63f805a20c79c5b4ed44db62a728d5a2558
Gessler Pedrol <gpedr012@fiu.edu>
2020-07-26 02:06:34 -0400

    Created control to select color when setting up game.

---
+++ src/checkers/ui/ColorToggle.java
@@ -0,0 +1,93 @@
+package checkers.ui;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class ColorToggle extends StackPane
+{
+    Piece pieceBg;
+    Rectangle bg = new Rectangle(60, 60);
+    BooleanProperty isSelected = new SimpleBooleanProperty(false);
+
+    public ColorToggle(Piece.PieceColor color)
+    {
+
+
+        bg.setFill(Color.DARKGRAY);
+        bg.setStrokeWidth(3);
+        bg.setArcHeight(20);
+        bg.setArcWidth(20);
+        pieceBg = new Piece(color);
+        pieceBg.setScaleX(0.75);
+        pieceBg.setScaleY(0.75);
+        getChildren().add(bg);
+        getChildren().add(pieceBg);
+        setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent mouseEvent)
+            {
+                if(!isSelected.get())
+                {
+                    isSelected.set(true);
+                    System.out.println("isSelected.get() = " + isSelected.get());
+
+                }
+            }
+        });
+
+        isSelected.addListener(new ChangeListener<Boolean>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldV, Boolean newV)
+            {
+                if(newV)
+                {
+                    pieceBg.setOpacity(100);
+                    bg.setStroke(Color.WHITE);
+
+                }
+                else
+                {
+                    pieceBg.setOpacity(50);
+                    bg.setStroke(Color.DARKGRAY);
+                }
+            }
+        });
+
+        setId("sethover");
+    }
+
+    public void setOtherToggle(ColorToggle otherToggle)
+    {
+        isSelectedProperty().addListener(new ChangeListener<Boolean>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observableValue, Boolean oldV, Boolean newV)
+            {
+                if(newV)
+                {
+                    otherToggle.isSelectedProperty().set(false);
+                }
+                else
+                {
+                    otherToggle.isSelectedProperty().set(true);
+                }
+
+            }
+        });
+
+    }
+
+    public BooleanProperty isSelectedProperty()
+    {
+        return isSelected;
+    }
+}
---
+++ src/checkers/ui/ColorToggleMenu.java
@@ -0,0 +1,43 @@
+package checkers.ui;
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+
+public class ColorToggleMenu extends HBox
+{
+    ColorToggle darkToggle;
+    ColorToggle lightToggle;
+
+    public ColorToggleMenu()
+    {
+        ColorToggle darkToggle = new ColorToggle(Piece.PieceColor.DARK);
+        ColorToggle lightToggle = new ColorToggle(Piece.PieceColor.LIGHT);
+
+        darkToggle.setOtherToggle(lightToggle);
+        lightToggle.setOtherToggle(darkToggle);
+
+        setAlignment(Pos.CENTER);
+        setSpacing(10);
+
+        this.darkToggle = darkToggle;
+        this.lightToggle = lightToggle;
+
+        getChildren().addAll(darkToggle, lightToggle);
+    }
+
+    public Piece.PieceColor getSelection()
+    {
+        if(darkToggle.isSelected.get())
+        {
+            return Piece.PieceColor.DARK;
+        }
+        else if(lightToggle.isSelected.get())
+        {
+            return Piece.PieceColor.LIGHT;
+        }
+        else
+        {
+            return Piece.PieceColor.DARK;
+        }
+    }
+}
