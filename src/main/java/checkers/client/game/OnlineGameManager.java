6a9f814503e28208c973fdd93e40be905cff437e
Gessler Pedrol <gpedr012@fiu.edu>
2021-03-18 19:42:14 -0400

    Added code to handle enemy movements online.

--- src/main/java/checkers/client/game/OnlineGameManager.java
+++ src/main/java/checkers/client/game/OnlineGameManager.java
@@ -1,28 +1,54 @@
 package checkers.client.game;
 
+import checkers.client.ui.Animator;
 import checkers.client.ui.Board;
+import checkers.client.ui.Tile;
+import checkers.networkutils.Action;
+import checkers.networkutils.Message;
 
 public class OnlineGameManager
 {
     private static Board board;
     private static Player player;
 
+
     private OnlineGameManager () {}
 
 
-    public static void updateTurnStatus(boolean turnStatus) {
-        player.setHasTurn(turnStatus);
-
-        if(turnStatus) {
-            player.processTurn();
-
-        }
+    public static void processPlayerTurn() {
+        player.processTurn();
 
     }
 
     public static void initialize(Board board, Player player) {
         OnlineGameManager.board = board;
         OnlineGameManager.player = player;
+
+    }
+
+    public static void processEnemyTurn(Action action) {
+        int[] actionArgs = action.getAllArgs();
+
+        int rowOrigin = actionArgs[Message.ROW_ORIGIN_IDX];
+        int colOrigin = actionArgs[Message.COL_ORIGIN_IDX];
+        int rowDest = actionArgs[Message.ROW_DEST_IDX];
+        int colDest = actionArgs[Message.COL_DEST_IDX];
+
+        Tile currentTile = board.getTile(rowOrigin, colOrigin);
+        Tile destinationTile = board.getTile(rowDest, colDest);
+
+        if(action.getType() == Action.Type.MOVE_ELIM) {
+            int enemyRow = actionArgs[Message.ROW_ELIM_IDX];
+            int enemyCol = actionArgs[Message.COL_ELIM_IDX];
+
+            Tile enemyTile = board.getTile(enemyRow, enemyCol);
+
+            Animator.playMovementAnimation(board, currentTile, destinationTile, enemyTile);
+
+        } else {
+            Animator.playMovementAnimation(board, currentTile, destinationTile);
+
+        }
 
     }
 }
